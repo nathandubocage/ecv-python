dd32525e1fc27f2bb1491688acf68c4256d8d8fc
Nathan Dubocage <nathandubocage@gmail.com>
2023-04-21 12:01:11 +0200

    Suppression de l'ancien front

--- front/formulaire_app/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class MonFormulaire(forms.Form):
-    champ1 = forms.CharField(label="Champ 1", max_length=100)
-    champ2 = forms.IntegerField(label="Champ 2")
-    # Ajoutez d'autres champs si nécessaire
